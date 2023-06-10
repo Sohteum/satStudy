89d019f564b85c01d65b73be187589dbf429306b
Sohteum <thtwk1123@gmail.com>
2023-06-11 07:48:52 +0900

    .

--- JavsScript/230610/선생님설명/script.js
+++ JavsScript/230610/선생님설명/script.js
@@ -7,12 +7,24 @@ const fnEx2 = (param1, param2) => {
   document.querySelector('.ex3 div').innerText = param2.target.value
   console.log(param1)
 }
+const fnEx3 = (param1, param2) => {
+  document.querySelector('.ex3 div').innerText = param2.target.value
+  console.log(param1)
+}
+
+
 
 document.addEventListener('DOMContentLoaded',()=>{
 
   document.querySelector('.ex1 button').addEventListener('click',(z,c)=>{
     alert(z.target.value)
     console.log(c);
+  })
+
+  document.querySelector(`.ex3 button`).addEventListener('click', e=>{
+document.querySelectorAll(`.ex3 button`).forEach(button=>{
+  
+})
   })
 
 })
