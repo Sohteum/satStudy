5fa8a81ae916c93c270926fec5e23041909f9414
Sohteum <thtwk1123@gmail.com>
2023-06-18 23:26:28 +0900

    .

--- JavsScript/230617/script.js
+++ JavsScript/230617/script.js
@@ -12,3 +12,15 @@ document.querySelector('.check').addEventListener('click', function () {
 
   }
 })
+
+document.querySelector('.check').addEventListener('click', (e)=>{
+
+
+if (number % 10 === 3 || number % 10 === 6 || number % 10 === 9) {
+  console.log('박수'); 
+  answer.innerHTML = '박수';
+} else {
+  console.log('통과');
+  answer.innerHTML = '통과';
+
+})
